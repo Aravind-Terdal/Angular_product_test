3de74306a37ae1f249e70b0cba6cde7c2ed329c7
Aravind Terdal <aravindterdal@gmail.com>
2025-04-27 16:35:06 +0530

    nochanges

--- src/app/shared/components/products-home/products-home.component.ts
+++ src/app/shared/components/products-home/products-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
-import { Iproduct } from '../../modules/products';
+import { Iproduct } from '../../models/products';
 
 @Component({
   selector: 'app-products-home',
